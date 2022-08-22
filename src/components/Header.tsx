8c89ea789b4be4caae824542207f3340ab9f71d5
keigrey <keixgrey@gmail.com>
2022-08-22 16:50:57 +0900

    feat: Add navigation to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
       <h1>HEADER</h1>
+      <Link to="/register">Register</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/home">Home</Link>
     </header>
   );
 };
