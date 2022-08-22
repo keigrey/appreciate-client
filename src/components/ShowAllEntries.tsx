975e46206f7fe673243caa98861c71b0194bfdea
keigrey <keixgrey@gmail.com>
2022-08-22 17:59:34 +0900

    feat: Display entries from local variable

--- src/components/ShowAllEntries.tsx
+++ src/components/ShowAllEntries.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Entry from "./Entry";
 
 const ShowAllEntries = () => {
-  return <div>ShowAllEntries</div>;
+  const [entries, setEntries] = useState<
+    {
+      date: string;
+      title: string;
+      text: string;
+    }[]
+  >([]);
+
+  const list = [
+    {
+      date: "2022-08-22",
+      title: "asdas",
+      text: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    },
+    {
+      date: "2016-01-06",
+      title: "NExt",
+      text: "ASDKLASLKDHALKSDHJLKASHJD",
+    },
+    {
+      date: "2036-10-15",
+      title: "mmmmmm",
+      text: "sad234234234234234",
+    },
+  ];
+
+  useEffect(() => {
+    setEntries(list);
+  }, []);
+
+  const displayAllEntries = entries.map((entry) => (
+    <Entry date={entry.date} title={entry.title} text={entry.text} />
+  ));
+
+  return (
+    <div>
+      <h1>Show all</h1>
+      <div>{displayAllEntries}</div>
+    </div>
+  );
 };
 
 export default ShowAllEntries;
