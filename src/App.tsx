6fddfdd3a20f7790425720ff7b9e5d8b5fede9aa
keigrey <keixgrey@gmail.com>
2022-08-22 14:38:06 +0900

    feat: Add routes to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import AddEntry from "./components/AddEntry";
 import Header from "./components/Header";
 import Login from "./components/Login";
@@ -7,15 +8,21 @@ import ShowAllEntries from "./components/ShowAllEntries";
 
 function App() {
   return (
-    <div className="App">
-      Hello REact
-      <Header />
-      <h1>--------------------</h1>
-      <Register />
-      <Login />
-      <AddEntry />
-      <ShowAllEntries />
-    </div>
+    // <div className="App">
+    //   Hello REact
+    //   <Header />
+    //   <h1>--------------------</h1>
+    //   <Register />
+    //   <Login />
+    //   <AddEntry />
+    //   <ShowAllEntries />
+    // </div>
+    <Routes>
+      <Route path="/" element={<Header />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/myhome" element={<AddEntry />} />
+    </Routes>
   );
 }
 
