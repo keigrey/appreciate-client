e6368572d0ed57297c1bf319a22e9af6892e3515
keigrey <keixgrey@gmail.com>
2022-08-22 18:00:15 +0900

    feat: Get form data from Login/Register forms

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,23 +1,36 @@
 import React from "react";
 import Header from "./Header";
 import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+type LoginInfo = {
+  email: string;
+  password: string;
+};
 
 const Login = () => {
   const navigate = useNavigate();
+  const { register, handleSubmit } = useForm<LoginInfo>();
+
+  const onSubmit = handleSubmit((data) => {
+    // alert(JSON.stringify(data));
+    console.log(data);
+  });
+
   return (
     <div>
       <Header />
       <h1>Login</h1>
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          navigate("/home");
-        }}
-      >
+      <form onSubmit={onSubmit}>
         <label htmlFor="email">Email</label>
-        <input type="text" id="email" name="email" />
+        <input {...register("email")} type="text" id="email" name="email" />
         <label htmlFor="password">Password</label>
-        <input type="text" id="password" name="password" />
+        <input
+          {...register("password")}
+          type="text"
+          id="password"
+          name="password"
+        />
         <input type="submit" value="Login" />
       </form>
     </div>
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,22 +1,60 @@
 import React from "react";
 import Header from "./Header";
+import { useForm } from "react-hook-form";
+
+type RegisterInfo = {
+  first_name: string;
+  last_name: string;
+  email: string;
+  password: string;
+  confirm_password: string;
+};
 
 const Register = () => {
+  const { register, handleSubmit } = useForm<RegisterInfo>();
+
+  const onSubmit = handleSubmit((data) => {
+    // alert(JSON.stringify(data));
+    console.log(data);
+  });
+
   return (
     <div>
       <Header />
       <h1>Register</h1>
-      <label htmlFor="first_name">First Name</label>
-      <input type="text" id="first_name" name="first_name" />
-      <label htmlFor="last_name">Last Name</label>
-      <input type="text" id="last_name" name="last_name" />
-      <label htmlFor="email">Email</label>
-      <input type="text" id="email" name="email" />
-      <label htmlFor="password">Password</label>
-      <input type="text" id="password" name="password" />
-      <label htmlFor="confirm_password">Confirm Password</label>
-      <input type="text" id="confirm_password" name="confirm_password" />
-      <input type="submit" value="Register" />
+      <form onSubmit={onSubmit}>
+        <label htmlFor="first_name">First Name</label>
+        <input
+          {...register("first_name")}
+          type="text"
+          id="first_name"
+          name="first_name"
+        />
+        <label htmlFor="last_name">Last Name</label>
+        <input
+          {...register("last_name")}
+          type="text"
+          id="last_name"
+          name="last_name"
+        />
+        <label htmlFor="email">Email</label>
+        <input {...register("email")} type="text" id="email" name="email" />
+        <label htmlFor="password">Password</label>
+        <input
+          {...register("password")}
+          type="text"
+          id="password"
+          name="password"
+        />
+        <label htmlFor="confirm_password">Confirm Password</label>
+        <input
+          {...register("confirm_password")}
+          type="text"
+          id="confirm_password"
+          name="confirm_password"
+        />
+        <input type="submit" value="Register" />
+      </form>
     </div>
   );
 };
