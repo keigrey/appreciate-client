4ede1840fbdde94f50f4e2bc7a6afb53bf3a2bde
keigrey <keixgrey@gmail.com>
2022-10-26 21:09:34 +0900

    style: Add style to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,17 +4,31 @@ import Logout from "./Logout";
 
 const Header = () => {
   return (
-    <header>
-      <h1>HEADER</h1>
+    <header className="bg-accent p-2 flex justify-between">
       {localStorage.getItem("JWToken") ? (
         <>
-          <Link to="/home">Home</Link>
+          <Link
+            className="bg-dark hover:bg-accentdarker text-main mr-4 py-2 px-4 rounded-sm"
+            to="/home"
+          >
+            Home
+          </Link>
           <Logout />
         </>
       ) : (
         <>
-          <Link to="/register">Register</Link>
-          <Link to="/login">Login</Link>
+          <Link
+            className="bg-dark hover:bg-accentdarker text-main mr-4 py-2 px-4 rounded-sm"
+            to="/register"
+          >
+            Register
+          </Link>
+          <Link
+            className="bg-dark hover:bg-accentdarker text-main mr-4 py-2 px-4 rounded-sm"
+            to="/login"
+          >
+            Login
+          </Link>
         </>
       )}
     </header>
