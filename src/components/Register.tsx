b549f8424dc6363a2153e669773086213ebbc430
keigrey <keixgrey@gmail.com>
2022-10-26 21:10:10 +0900

    style: Add style to Register

--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -10,7 +10,8 @@ type RegisterInfo = {
   password: string;
   confirm_password: string;
 };
-const SERVER_URL = "https://app-reciate.herokuapp.com";
+// const SERVER_URL = "https://app-reciate.herokuapp.com";
+const SERVER_URL = "http://localhost:8080";
 // const SERVER_URL = process.env.PORT
 //   ? "https://app-reciate.herokuapp.com/"
 //   : "http://localhost:8080";
@@ -26,52 +27,95 @@ const Register = () => {
   });
 
   return (
-    <div>
+    <div className="w-full h-[calc(100vh-60px)]">
       <Header />
-      <h1>Register</h1>
-      <form onSubmit={onSubmit}>
-        <label htmlFor="first_name">First Name</label>
-        <input
-          {...register("first_name")}
-          type="text"
-          id="first_name"
-          name="first_name"
-          required
-        />
-        <label htmlFor="last_name">Last Name</label>
-        <input
-          {...register("last_name")}
-          type="text"
-          id="last_name"
-          name="last_name"
-          required
-        />
-        <label htmlFor="email">Email</label>
-        <input
-          {...register("email")}
-          type="text"
-          id="email"
-          name="email"
-          required
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          {...register("password")}
-          type="text"
-          id="password"
-          name="password"
-          required
-        />
-        <label htmlFor="confirm_password">Confirm Password</label>
-        <input
-          {...register("confirm_password")}
-          type="text"
-          id="confirm_password"
-          name="confirm_password"
-          required
-        />
-        <input type="submit" value="Register" />
-      </form>
+      <div className=" h-full  flex justify-center">
+        <form
+          className="bg-submain w-96 h-4/5 shadow-md pt-6 pb-8 pl-6 pr-8 flex flex-col justify-center content-center self-center rounded"
+          onSubmit={onSubmit}
+        >
+          <label
+            className="block text-dark text-lg font-bold mb-2"
+            htmlFor="first_name"
+          >
+            First Name
+          </label>
+          <input
+            {...register("first_name")}
+            className="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline "
+            type="text"
+            id="first_name"
+            name="first_name"
+            placeholder="Toshiro"
+            required
+          />
+          <label
+            className="block text-dark text-lg font-bold mb-2 mt-4"
+            htmlFor="last_name"
+          >
+            Last Name
+          </label>
+          <input
+            {...register("last_name")}
+            className="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline "
+            type="text"
+            id="last_name"
+            name="last_name"
+            placeholder="Mifune"
+            required
+          />
+          <label
+            className="block text-dark text-lg font-bold mb-2 mt-4"
+            htmlFor="email"
+          >
+            Email
+          </label>
+          <input
+            {...register("email")}
+            className="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline "
+            type="text"
+            id="email"
+            name="email"
+            placeholder="example@example.com"
+            required
+          />
+          <label
+            className="block text-dark text-lg font-bold mb-2 mt-4"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            {...register("password")}
+            className="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            type="password"
+            id="password"
+            name="password"
+            placeholder="Pa$$word!@123"
+            required
+          />
+          <label
+            className="block text-dark text-lg font-bold mb-2 mt-4"
+            htmlFor="confirm_password"
+          >
+            Confirm Password
+          </label>
+          <input
+            {...register("confirm_password")}
+            className="shadow appearance-none rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-6"
+            type="password"
+            id="confirm_password"
+            name="confirm_password"
+            placeholder="Pa$$word!@123"
+            required
+          />
+          <input
+            className="cursor-pointer w-full bg-dark hover:bg-accentdarker text-main mr-4 py-2 px-4 rounded-sm"
+            type="submit"
+            value="Register"
+          />
+        </form>
+      </div>
     </div>
   );
 };
