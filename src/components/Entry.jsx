775407367210e6ea5e0256bb0701fb45302dcc27
keigrey <keixgrey@gmail.com>
2022-10-26 18:48:57 +0900

    style: Add style to Entry

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const Entry = ({ date, title, text }) => {
   return (
-    <div>
-      <h1>------------------Entry-------------</h1>
-      <h2>{date}</h2>
-      <h2>{title}</h2>
-      <h2>{text}</h2>
+    <div className="card bg-submain m-8 p-8 max-w-sm rounded shadow-lg">
+      <div className="container break-words">
+        <h1 className="mb-3 font-bold text-dark text-xl">{title}</h1>
+        <p className="text-dark text-base">{text}</p>
+        <h2 className="mt-3 text-right">{date}</h2>
+      </div>
     </div>
   );
 };
