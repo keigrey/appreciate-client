b952f5e01a717499df3db2f562b71853a7fdc338
keigrey <keixgrey@gmail.com>
2022-10-26 18:48:25 +0900

    chore: Add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      main: "#F0EBE3",
+      submain: "#E4DCCF",
+      accent: "#7D9D9C",
+      accentdarker: "#134e4a",
+      dark: "#576F72",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
